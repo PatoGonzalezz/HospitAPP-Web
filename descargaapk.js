265fe3466ee82a0ed80a3a5285f55c950c629c0d
PatoGonzalezz <pato12lc@gmail.com>
2023-12-14 20:54:09 -0300

    termiado

---
+++ descargaapk.js
@@ -0,0 +1,7 @@
+function abrirEnlace() {
+    // URL del archivo que quieres descargar
+    var url = 'apk/HospitAPP.apk';
+    
+    // Abrir el enlace en una nueva ventana o pestaña
+    window.open(url, '_blank');
+}
